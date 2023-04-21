7213728b7a8b2e5ced09766599c8d9eeb295d63b
cheeter_Lee <johnson991210@gmail.com>
2023-04-21 12:21:31 +0800

    update export code

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,13 @@ import '../tailwind.css'
 export { default as Button } from './Button/Button'
 export { default as Heading } from './Heading/Heading'
 export { default as Text } from './Text/Text'
+/* Card */
 export { default as Card } from './Card/Card'
 export { default as CardBody } from './Card/CardBody'
 export { default as CardHeader } from './Card/CardHeader'
+/* Tab */
+export { TabContextProvider } from './Tab/TabContext'
+export { default as Tab } from './Tab/Tab'
+export { default as TabList } from './Tab/TabList'
+export { default as TabPanel } from './Tab/TabPanel'
+/* Popover */
